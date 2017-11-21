b18fbba75c09a1fe99cb2307fe6d2c3429537f40
krishal99 <7216288+krishal99@users.noreply.github.com>
2017-11-21 18:11:31 +0000

    Additional Functionality
    adds save/load functions

--- setup.h
+++ setup.h
@@ -1,18 +1,39 @@
+struct playerStats player1;
 void setup()
 {
-    struct playerStats player1;
+    int choice;
+    printf("Would you like to load a previous save (y or n)?\n", saveDefaultColor());
+    scanf("%c", &choice);
+    if (choice== 'y')
+    {
+        load();
+        init();
+    }
     system("cls");
-    printf("Choose a name:\n", saveDefaultColor());
+    printf("Choose a name:\n");
     scanf("%s", &player1.name);
-    printf("\n\nOkay, %s, you are granted 20 points to divide between Attack, \nDefence and Health.", player1.name);
-    getch();
+    setup2();
+}
+void setup2()
+{
+    int choice;
+    system("cls");
+    printf("Okay, %s, you are granted 20 points to divide between Attack, \nDefence and Health.", player1.name);
     printf("\n\nLet's start with the Attack stat. How many points into Attack?\n");
     scanf("%i", &player1.atk);
     player1.spts = 20 - player1.atk;
-    printf("\nNow for the Defence stat:\n");
+    printf("\nYou have ");
+    setColor(14);
+    printf("%i",player1.spts);
+    resetColor();
+    printf(" points left.\n\nNow for the Defence stat:\n");
     scanf("%i", &player1.def);
     player1.spts = player1.spts - player1.def;
-    printf("\nAnd finally, the Health stat:\n");
+    printf("\nYou have ");
+    setColor(14);
+    printf("%i", player1.spts);
+    resetColor();
+    printf(" points left. \nLastly, the HP stat:\n");
     scanf("%i", &player1.hp);
     player1.spts = player1.spts - player1.hp;
     if (player1.spts < 0)
@@ -21,7 +42,7 @@ void setup()
         printf("\nError! Too many points allocated! Start again!");
         resetColor();
         getch();
-        setup();
+        setup2();
     }
     if (player1.spts > 0)
     {
@@ -29,9 +50,17 @@ void setup()
         printf("\nError! Not all points allocated! Start again!");
         getch();
         resetColor();
-        setup();
-
+        setup2();
     }
-    
+    setColor(12);
+    printf("\nWould you like to save your stats?(y or n)?\n");
+    resetColor();
+    scanf(" %c", &choice);
+    if (choice == 'y')
+    {
+        save();
+        init();
+    }
     init();
+    
 }
