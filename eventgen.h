04fe0386abb84930110a48fd58f52cded008a079
krishal99 <7216288+krishal99@users.noreply.github.com>
2017-12-06 21:11:43 +0000

    Mob generator ref
    added reference to mob generator, optimised for linux

--- eventgen.h
+++ eventgen.h
@@ -1,17 +1,34 @@
-void eventgen()
+void eventgen(int choice)
 {
-    int event = rand() % 10;
+    int event = rand() % 100;
     int twn = rand() % 10;
     char town[10][100] = {"Scream", "Tatter", "Terry", "Dumblin", "Tree", "Pigmen", "Woop", "Hippie", "AAAAAAAA", "Lavender"};
+    char city[10][100] = {"Scream", "Tatter", "Terry", "Dumblin", "Tree", "Pigmen", "Woop", "Hippie", "AAAAAAAA", "Lavender"};
     
-    switch (event)
+    switch (event)//switch which decides event
     {
         case 2:
         printf("\nYou find %s town!", town[twn]);
         break;
         
+        case 12:
+        printf("\nYou find %s town!", town[twn]);
+        break;
+        
+        case 27:
+        printf("\nYou find %s town!", town[twn]);
+        break;
+        
+        case 43:
+        printf("\nYou find %s town!", town[twn]);
+        break;
+        
+        case 4:
+        printf("\nYou find %s City!", city[twn]);
+        break;
+        
         default:
-        printf("\nYou find nothing.");
+        mobtype(choice);
         break;
 
     }
