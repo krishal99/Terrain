d56de172e975e9d4f317522a1c947d44650edf13
krishal99 <7216288+krishal99@users.noreply.github.com>
2017-12-14 12:44:53 +0000

    revision of stats
    better stats system, levels instead of literally using points as statpoints... plus starting stats are a thing now

--- setup.h
+++ setup.h
@@ -16,26 +16,35 @@ void setup()
 }
 void setup2()
 {
+    int atk, def, hp;
     char choice;
     cls();
-    printf("Okay, %s, you are granted 20 points to divide between Attack, \nDefence and Health.", player1.name);
+    printf("Okay, %s, you are granted ", player1.name);
+    setColor(14);
+    printf("10");
+    resetColor();
+    printf(" points to divide between Attack, \nDefence and Health.");
     printf("\n\nLet's start with the Attack stat. How many points into Attack?\n");
-    scanf("%i", &player1.atk);
-    player1.spts = 20 - player1.atk;
+    scanf("%i", &atk);
+    player1.atk = 10 + (2*atk);
+    player1.spts = 10 - atk;
     printf("\nYou have ");
     setColor(14);
     printf("%i",player1.spts);
     resetColor();
     printf(" points left.\n\nNow for the Defence stat:\n");
-    scanf("%i", &player1.def);
-    player1.spts = player1.spts - player1.def;
+    scanf("%i", &def);
+    player1.def = 5 + (2*def);
+    player1.spts = player1.spts - def;
     printf("\nYou have ");
     setColor(14);
     printf("%i", player1.spts);
     resetColor();
     printf(" points left. \nLastly, the HP stat:\n");
-    scanf("%i", &player1.hp);
-    player1.spts = player1.spts - player1.hp;
+    scanf("%i", &hp);
+    player1.hp = 20 + (5*hp);
+    player1.spts = player1.spts - hp;
+    printf("\nYour stats are as follows:\nATK:\t%i\nDEF:\t%i\nHP:\t%i\n\n", player1.atk, player1.def, player1.hp);
     if (player1.spts < 0)
     {
         setColor(12);
