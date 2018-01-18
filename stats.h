227a4212fde5336ca927d77c25760aa2411371a2
krishal99 <7216288+krishal99@users.noreply.github.com>
2018-01-18 13:14:51 +0000

    Small Update
    to accomodate the new xp system

--- stats.h
+++ stats.h
@@ -1,11 +1,11 @@
 struct playerStats
 {
     char name[100];
-    int hp, def, atk, lvl, spts;
+    int hp, def, atk, lvl, spts, xp;
 };
 
 struct enemyStats
 {
-    char ename;
+    char ename[100], tname[100];
     int hp, def, atk, lvl;
 };
