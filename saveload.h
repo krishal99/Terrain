d8dd1cb17ddff44a80e9f2494fd445213db671aa
krishal99 <7216288+krishal99@users.noreply.github.com>
2018-01-19 11:22:41 +0000

    Added new function
    to delete save

--- saveload.h
+++ saveload.h
@@ -5,14 +5,15 @@ void load()
     if (fptr == NULL)
     {
         setColor(12);
-        printf("\nFile read failed!");
+        printf("\nFile read failed!\n");
+        resetColor();
         setup();
     }
     else (setColor(10));
     printf("\nFile load successful!");
     getch();
     resetColor();
-    fscanf(fptr, "%s %i %i %i %i %i", &player1.name, &player1.atk, &player1.def, &player1.hp, &player1.lvl, &player1.spts);
+    fscanf(fptr, "%s %i %i %i %i %i %i", &player1.name, &player1.atk, &player1.def, &player1.hp, &player1.lvl, &player1.spts, &player1.xp);
     fclose(fptr);
     }
     
@@ -27,9 +28,30 @@ void save()
         exit(-1);
     }
     else (setColor(10));
-    printf("\nFile save successful!");
-    getch();
+    //printf("\nFile save successful!");
+    //getch();
     resetColor();
-    fprintf(fps, "%s %i %i %i %i %i", player1.name, player1.atk, player1.def, player1.hp, player1.lvl, player1.spts);
+    fprintf(fps, "%s %i %i %i %i %i %i", player1.name, player1.atk, player1.def, player1.hp, player1.lvl, player1.spts, player1.xp);
     fclose(fps);
     }
+
+void savedel()
+{
+    int ret;
+    char filename[] = "stats.txt";
+    
+    ret = remove(filename);
+    
+    if (ret == 0)
+    {
+        printf("\nSave Deleted.\n\n");
+        resetColor();
+    }
+    else
+    {
+        printf("\nDelete Failed.");
+    }
+    exit(0);
+}
+
+
